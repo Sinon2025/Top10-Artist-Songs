6336327ecce2c486ec0736a9929c17505b5563af
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2019-08-25 00:34:40 +0200

    functionality added

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const takeInfo=(artistInfo)=>{
-    console.log(artistInfo);
-    
+    const artistName = artistInfo[0]["result"]["primary_artist"]["name"];
+
     const fullTitle = artistInfo.map(title => title["result"]["full_title"])
     
     const artistImageUrl = artistInfo[0]["result"]["primary_artist"]["image_url"];
@@ -9,13 +9,91 @@ const takeInfo=(artistInfo)=>{
     const textSongUrl = artistInfo.map(textUrl => textUrl["result"]["url"]);
 
     return {
+        "name": artistName,
         "fullTitle": fullTitle,
         "artistImageUrl": artistImageUrl,
         "textSongUrl": textSongUrl,
     }
 }
 
+const addArtistName=(date)=>{
+     const h2Artist = document.createElement("h2")
+     const artistNode = document.createTextNode(date);
+     h2Artist.appendChild(artistNode);
+     return h2Artist
+}
 
+const addArtistImage=(date)=>{
+    const imageArtist = document.createElement("img");
+    imageArtist.id = "artist_photo";
+    imageArtist.src = date["artistImageUrl"];
+    
+    return imageArtist
+
+};
+
+
+const addSongs=(date)=>{
+   console.log(date["fullTitle"]);
+    const section=document.getElementsByClassName("result-box")
+    const allPindex=[];
+    for (index in date["fullTitle"]){
+        const pIndex=document.createElement('p');
+        pIndex.className="index";
+        const nodeP = document.createTextNode(parseInt(index) + 1);
+        pIndex.appendChild(nodeP);
+        allPindex.push(pIndex);
+        // console.log(pIndex);
+    }
+    const allPtitle = date["fullTitle"].map(title=>{
+        const pIndex = document.createElement('p');
+        pIndex.className = "title";
+        const nodeP = document.createTextNode(title);
+        pIndex.appendChild(nodeP);
+        return pIndex;
+        
+    });
+
+    const allPlyrics = date["textSongUrl"].map(lyrics => {
+        const aUrl = document.createElement('a');
+        aUrl.className = "lyrics";
+        aUrl.href = lyrics;
+        const nodeA = document.createTextNode("Lyrics");
+        aUrl.appendChild(nodeA);
+        
+        return aUrl
+    });
+
+
+    section[0].appendChild(addArtistName(date["name"]));
+    section[0].appendChild(addArtistImage(date));
+
+    for (let i in allPindex){
+        const divSong = document.createElement("div");
+        divSong.className = "song-box"; 
+        divSong.appendChild(allPindex[i]);
+        divSong.appendChild(allPtitle[i]);
+        divSong.appendChild(allPlyrics[i]);
+         section[0].appendChild(divSong)   
+    }
+    section[0].appendChild(addArtistName(date["name"]));
+    section[0].appendChild(addArtistImage(date));
+
+};
+
+const searchArtist=()=>{
+    const searchbtn = document.querySelector(".btn-srch");
+     const input = document.querySelector(".artist_srch");
+    const search = (event) => {
+        event.preventDefault()
+        const inputValue = input.value;
+        artistRequest(inputValue);
+        
+    };
+
+    searchbtn.addEventListener('click', search);
+  
+}
 
 
 
@@ -31,15 +109,15 @@ const artistRequest=(aritstName="Sarius")=>{
         })
         .then(response => response.json()
         ).then(response => response["response"]["hits"]
-        ).then(response => console.log(takeInfo(response))
-        ).catch(err => {
+        ).then(response => {
+            const artist=takeInfo(response)
+            addSongs(artist);
+        }).catch(err => {
             console.log(err);
         });
 };
 
 const main=()=>{
-    console.log(0)
-    const dateArtis=artistRequest();
+    searchArtist()
 }
-
 main()
