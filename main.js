8e889534f4a73b9341a19c86092921100356c97d
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2019-08-25 01:36:51 +0200

    refresh functionality

--- main.js
+++ main.js
@@ -27,11 +27,17 @@ const addArtistImage=(date)=>{
     const imageArtist = document.createElement("img");
     imageArtist.id = "artist_photo";
     imageArtist.src = date["artistImageUrl"];
-    
     return imageArtist
-
 };
-
+const changeSection = () => {
+    const sect = document.querySelector(".result-box");
+    sect.parentNode.removeChild(sect);
+     const creatSect = document.createElement("section");
+     const body = document.querySelector("body");
+     const footer= body.querySelector("footer");
+     creatSect.className = "result-box";
+    body.insertBefore(creatSect, footer);
+}
 
 const addSongs=(date)=>{
    console.log(date["fullTitle"]);
@@ -63,8 +69,6 @@ const addSongs=(date)=>{
         
         return aUrl
     });
-
-
     section[0].appendChild(addArtistName(date["name"]));
     section[0].appendChild(addArtistImage(date));
 
@@ -76,8 +80,6 @@ const addSongs=(date)=>{
         divSong.appendChild(allPlyrics[i]);
         section[0].appendChild(divSong)   
     }
-    
-
 };
 
 const searchArtist=()=>{
@@ -85,15 +87,12 @@ const searchArtist=()=>{
      const input = document.querySelector(".artist_srch");
     const search = (event) => {
         event.preventDefault()
+        changeSection();
         const inputValue = input.value;
-        artistRequest(inputValue);
-        
+        artistRequest(inputValue);     
     };
-
-    searchbtn.addEventListener('click', search);
-  
+    searchbtn.addEventListener('click', search); 
 }
-
 const artistRequest=(aritstName="Sarius")=>{
     const link = "https://genius.p.rapidapi.com/search?q=";
     const url = link + aritstName;
@@ -107,7 +106,8 @@ const artistRequest=(aritstName="Sarius")=>{
         .then(response => response.json()
         ).then(response => response["response"]["hits"]
         ).then(response => {
-            const artist=takeInfo(response)
+            const artist=takeInfo(response);
+            
             addSongs(artist);
         }).catch(err => {
             console.log(err);
